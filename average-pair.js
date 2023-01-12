cf9ed0fa013c000799ca4f5612e800fd81e84b23
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-12 15:29:47 -0500

    finished avg-pair exercise

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(sortedArr, targetAvg) {
+	let start = 0;
+	let end = sortedArr.length - 1;
+
+	while (start < end) {
+		let avg = (sortedArr[start] + sortedArr[end]) / 2;
+		if (avg === targetAvg) {
+			return true;
+		}
+		else if (avg < targetAvg) {
+			start++;
+		}
+		else {
+			end--;
+		}
+	}
+	return false;
 }
