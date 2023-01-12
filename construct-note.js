4f1db3e4762658ef7a5cd6735dac35dfc2d46721
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-12 14:56:43 -0500

    completed construct-note

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, givenLetters) {
+	const lettersFreq = {};
+	const messageFreq = {};
+
+	for (let char of givenLetters) {
+		lettersFreq[char] = ++lettersFreq[char] || 1;
+	}
+	for (let char of message) {
+		messageFreq[char] = ++messageFreq[char] || 1;
+	}
+	for (let char in messageFreq) {
+		if (!lettersFreq[char]) {
+			return false;
+		}
+		if (messageFreq[char] > lettersFreq[char]) {
+			return false;
+		}
+	}
+	return true;
 }
