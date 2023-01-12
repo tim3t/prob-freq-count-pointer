89f66a1ac99d03470158885abfa58c74e222c266
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-12 15:33:24 -0500

    completed two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,9 @@
+const { getRandomValues } = require('crypto');
+
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+	return arr1.reduce((obj, cur, idx) => {
+		obj[cur] = idx < arr2.lenght ? arr2[idx] : null;
+		return obj;
+	}, {});
 }
